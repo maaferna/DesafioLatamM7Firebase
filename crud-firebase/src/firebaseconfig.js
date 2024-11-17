2e17bebb72abec26e32d059bc8b75c7ef2b05d5f
MarcoAntonioParra <55238942+maaferna@users.noreply.github.com>
2024-11-17 17:35:51 -0300

    Update firebaseconfig.js
    Hidden credentials

--- crud-firebase/src/firebaseconfig.js
+++ crud-firebase/src/firebaseconfig.js
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAn9R7YfB4qUrXXnP8wH5C58r4EuZPSX0",
+  apiKey: "key",
   authDomain: "desafiolatamm7crudfirabase1.firebaseapp.com",
   projectId: "desafiolatamm7crudfirabase1",
   storageBucket: "desafiolatamm7crudfirabase1.firebasestorage.app",
   messagingSenderId: "128743566067",
   appId: "1:128743566067:web:559b3fe95e980b6806a586",
-  measurementId: "G-BF5BMVXC1W"
+  measurementId: "measure-id"
 };
 
 const app = initializeApp(firebaseConfig);
